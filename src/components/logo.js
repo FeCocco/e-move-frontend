1beae7f90521c88d2160ba55ce9d8a416fa5452d
FeCocco <felipegco@proton.me>
2025-08-28 16:35:13 -0300

    Done the initial CSS configuration

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,8 +1,10 @@
+import './logo.css'
+
 function Logo() {
     return (
-        <>
+        <div className="logo-container">
             <h1 className="logo"><span className="logo-e">e</span>-Move</h1>
-        </>
+        </div>
     )
 }
 
