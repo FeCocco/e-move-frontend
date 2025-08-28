905cc649c44dd8e155c6e425e4181b09877f2162
FeCocco <felipegco@proton.me>
2025-08-28 17:11:39 -0300

    Implementacao inicial para testes de um siste de login web

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Logo from "@/components/logo";
+import LoginStepper from "@/components/LoginStepper/LoginStepper";
 
 export default function Home() {
     return (
         <section>
             <Logo />
-
+            <LoginStepper />
         </section>
     );
 }
