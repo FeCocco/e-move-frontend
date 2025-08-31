8330910c9e463d38af1d1f85e166ea1382b23cff
FeCocco <felipegco@proton.me>
2025-08-31 04:31:50 -0300

    Feat: Implementacao inicial da tela de Cadastro

--- src/components/LoginStepper/LoginStepper.jsx
+++ src/components/LoginStepper/LoginStepper.jsx
@@ -1,12 +1,14 @@
 'use client';
 import { useState } from 'react';
-import { Card, CardHeader, CardDescription, CardContent } from "@/components/ui/card";
+import {Card, CardHeader, CardDescription, CardContent, CardTitle} from "@/components/ui/card";
 import Logo from "@/components/Logo/Logo";
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
 
 export default function LoginStepper() {
     const [step, setStep] = useState(1);
-    const [formData, setFormData] = useState({ email: '', senha: ''});
+    const [formData, setFormData] = useState({ email: '', senha: '', nome: ''});
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
 
@@ -28,7 +30,7 @@ export default function LoginStepper() {
                     </CardHeader>
                     <CardContent className="text-center flex-col flex">
                         <form onSubmit={() => {}} className="flex flex-col gap-4 text-left">
-                            <label htmlFor="email">Email: </label>
+                            <Label htmlFor="email">Email: </Label>
                             <input
                                 name="email"
                                 type="email"
@@ -38,7 +40,7 @@ export default function LoginStepper() {
                                 required
                                 className="w-full p-3 rounded-lg border border-white/30 bg-white/5 text-white placeholder:text-white/50 focus:outline-none focus:border-azul-claro focus:ring-2 focus:ring-azul-claro/50"
                             />
-                            <label htmlFor="senha">Senha:</label>
+                            <Label htmlFor="senha">Senha:</Label>
                             <input
                                 name="senha"
                                 type="password"
@@ -56,14 +58,45 @@ export default function LoginStepper() {
                             </button>
                         </form>
                         <Button variant="link" className="mt-3 text-azul-claro underline hover:text-azul-botao">Esqueci minha senha</Button>
-                        <Button variant="link" className="mt-3 text-azul-claro underline hover:text-azul-botao">Criar nova conta</Button>
+                        <Button variant="link" onClick={() => setStep(2)} className="mt-3 text-azul-claro underline hover:text-azul-botao">Criar nova conta</Button>
                         <button
                             type=""
-                            className="text-white uppercase bg-red-600 cursor-pointer rounded-xl py-3 px-6 text-base font-bold transition-all duration-300 ease-in-out mt-3 hover:-translate-y-0.5 hover:shadow-[0_4px_15px_rgba(0,255,255,0.4)]"
-                        >
+                            className="text-white uppercase bg-red-600 cursor-pointer rounded-xl py-3 px-6 text-base font-bold transition-all duration-300 ease-in-out mt-3 hover:-translate-y-0.5 hover:shadow-[0_4px_15px_rgba(0,255,255,0.4)]">
                             Acesso ao painel (DEV)
                         </button>
 
+                    </CardContent>
+                </>
+            )}
+            {step === 2 && (
+                <>
+                    <CardHeader className="text-center">
+                        <CardTitle className="text-2xl font-bold">Criar Conta</CardTitle>
+                        <CardDescription className="text-texto-claro/80 pt-2">
+                            Já é um membro?{' '}
+                            <Button variant="link" onClick={() => setStep(1)} className="p-0 h-auto text-azul-claro hover:underline">
+                                Login
+                            </Button>
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form className="flex flex-col gap-2 text-left">
+                            <Label htmlFor="nome">Nome Completo</Label>
+                            <Input id="nome" name="nome" type="text" placeholder="Seu nome completo" required onChange={handleInputChange} className="bg-white/5 border-white/30 placeholder:text-white/50 focus-visible:ring-azul-claro"/>
+
+                            <Label htmlFor="email_cadastro" className="mt-4">Email</Label>
+                            <Input id="email_cadastro" name="email" type="email" placeholder="seu@email.com" required onChange={handleInputChange} className="bg-white/5 border-white/30 placeholder:text-white/50 focus-visible:ring-azul-claro"/>
+
+                            <Label htmlFor="senha_cadastro" className="mt-4">Senha</Label>
+                            <Input id="senha_cadastro" name="senha" type="password" placeholder="Crie uma senha" required onChange={handleInputChange} className="bg-white/5 border-white/30 placeholder:text-white/50 focus-visible:ring-azul-claro"/>
+
+                            <button
+                                type="submit"
+                                className="text-white uppercase bg-azul-botao cursor-pointer rounded-xl py-3 px-6 text-base font-bold transition-all duration-300 ease-in-out mt-3 hover:-translate-y-0.5 hover:shadow-[0_4px_15px_rgba(0,255,255,0.4)]"
+                            >
+                                Cadastrar
+                            </button>
+                        </form>
                     </CardContent>
                 </>
             )}
