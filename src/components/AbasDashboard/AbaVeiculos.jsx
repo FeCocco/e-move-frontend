a6229484dfbefe47d6f98e2eaaa9d357820ef5e9
FeCocco <felipegco@proton.me>
2025-09-16 14:52:36 -0300

    Update: Modularização das abas do Dashboard

---
+++ src/components/AbasDashboard/AbaVeiculos.jsx
@@ -0,0 +1,8 @@
+export default function AbaVeiculos(){
+    return(
+        <div>
+            <h2 className="text-2xl font-orbitron text-verde-claro mb-4">Meus Veículos</h2>
+            <p>Conteúdo da aba de veículos aqui...</p>
+        </div>
+    )
+}
