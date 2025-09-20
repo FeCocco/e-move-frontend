b985181312c1f4f7a81d65b1f8a494b9c453b23c
FeCocco <felipegco@proton.me>
2025-09-19 23:33:06 -0300

    Feat: Testes iniciais da exibição do mapa no frontend

--- src/components/AbasDashboard/AbaMapa.jsx
+++ src/components/AbasDashboard/AbaMapa.jsx
@@ -1,5 +1,26 @@
+"use client";
+import { useEffect } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 export default function AbaMapa() {
+    useEffect(() => {
+        const map = new maplibregl.Map({
+            container: 'map', // container id
+            style: 'https://demotiles.maplibre.org/style.json',
+            center: [0, 0],
+            zoom: 1
+        });
+
+        return () => map.remove(); // remove o mapa ao desmontar o componente
+    }, []);
+
     return (
-        <p>Conteúdo de Planejar Rota...</p>
-    )
+        <>
+            <p>Conteúdo de Planejar Rota...</p>
+            <div>
+                <div id="map" style={{ width: '100%', height: '400px' }} />
+            </div>
+        </>
+    );
 }
