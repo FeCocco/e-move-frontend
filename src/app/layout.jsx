352a50dd5b8697fd83db56fb11d3b03999c0b4ba
Felipe Giacomini Cocco <felipegco@proton.me>
2026-01-08 22:19:14 -0300

    Refactor styles and layout: merge CSS layers, update gradient background, and simplify component imports.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,6 @@ import { Orbitron, Poppins, Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Logo from "../components/Logo/Logo";
 import Link from "next/link";
-import Header from "@/components/Header/Header";
-import { usePathname } from 'next/navigation';
 import {Toaster} from "sonner";
 
 const orbitron = Orbitron({ subsets: ["latin"], weight: ["500", "700"], variable: "--font-orbitron" });
@@ -13,7 +11,6 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["300", "400", "500", "600
 const josefinSans = Josefin_Sans({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-josefin-sans" });
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname();
 
     return (
         <html lang="pt-br">
@@ -21,13 +18,13 @@ export default function RootLayout({ children }) {
             <title>e-Move</title>
             <meta name="description" content="Plataforma de mobilidade elétrica" />
         </head>
-        <body className={`${orbitron.variable} ${poppins.variable} ${josefinSans.variable} font-poppins`}>
+        <body className={`${orbitron.variable} ${poppins.variable} ${josefinSans.variable} font-poppins bg-gradient-body`}>
 
         <Link href="/" className="hidden sm:block">
-            <Logo className="fixed top-[25px] left-[30px] z-[1002] text-[1.6rem] text-azul-claro/70 drop-shadow-[0_0_8px_rgba(0,255,255,0.4)] cursor-pointer" />
+            <Logo className="fixed top-[25px] left-[30px] z-[1002] text-[1.6rem] text-azul-claro/70 drop-shadow-[0_0_8px_rgba(0,255,255,0.4)] cursor-pointer backdrop-blur-md rounded-xl p-2" />
         </Link>
 
-        <main className="min-h-screen w-full bg-gradient-body flex flex-col">
+        <main className="min-h-screen w-full flex flex-col">
             {children}
             <Toaster richColors position="top-right" />
         </main>
